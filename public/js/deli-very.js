e0aee7f5ef7b91c82673b9add0316a5deb423b90
Martin Eriksson <woodauncher+git@gmail.com>
2019-02-13 15:56:37 +0100

    Done with task 5-2

--- public/js/deli-very.js
+++ public/js/deli-very.js
@@ -2,9 +2,7 @@
 /* global Vue, io */
 /* exported vm */
 
-
 const socket = io();
-
 const vm = new Vue({
     el: 'main',
     data: {
@@ -14,38 +12,33 @@ const vm = new Vue({
         burgers,
     },
     created() {
-        socket.on('initialize', (data) => {
-            this.orders = data.orders;
-        });
+        // socket.on('initialize', (data) => {
+        //     this.orders = data.orders;
+        // });
 
-        socket.on('currentQueue', (data) => {
-            this.orders = data.orders;
-        });
+        // socket.on('currentQueue', (data) => {
+        //     this.orders = data.orders;
+        // });
     },
     methods: {
-        getNext() {
-            const lastOrder = Object.keys(this.orders).reduce((prev, next) => Math.max(prev, next), 100);
-            return lastOrder + 1;
-        },
-        addOrder(event) {
-            const offset = {
-                x: event.currentTarget.getBoundingClientRect().left,
-                y: event.currentTarget.getBoundingClientRect().top,
-            };
+        // getNext() {
+        //     const lastOrder = Object.keys(this.orders).reduce((prev, next) => Math.max(prev, next), 0);
+        //     return lastOrder + 1;
+        // },
+        addOrder() {
             const orderArray = [
                 ['Beans', 'Curry'],
                 ['Soup', 'Bread'],
                 ['Spaghetti', 'Meatballs'],
             ];
             const rand = Math.round(Math.random() * Math.floor(orderArray.length - 1));
-            socket.emit('addOrder', {
-                orderId: this.getNext(),
-                details: {
-                    x: event.clientX - 10 - offset.x,
-                    y: event.clientY - 10 - offset.y,
-                },
-                orderItems: orderArray[rand],
-            });
+
+
+            let order = this.orders[0];
+            // order.orderId = this.getNext(); // or still 'T' ????
+            order.orderItems = this.info.selectedBurgers.map(x => x.value);
+
+            socket.emit('addOrder', order);
         },
         displayOrder(event) {
             const offset = {
@@ -63,7 +56,14 @@ const vm = new Vue({
         },
         purchase_button_pressed() {
             console.log('Button clicked!');
-            this.info = readForm();
+            this.info = {
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                payment_options: document.getElementById('payment_options').value,
+                gender: document.querySelector('input[name="gender"]:checked').value,
+                selectedBurgers: [...document.querySelectorAll('#burger_menu input:checked')] /* .map(x => ` ${x.value}`) */ ,
+            };
+            this.addOrder();
             this.isShown = true;
         },
         formUpdate() {
