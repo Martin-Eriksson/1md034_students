4b41b005fd6563576699558c5991b2244bef4210
Martin Eriksson <woodauncher+git@gmail.com>
2019-02-13 16:45:40 +0100

    Done with task 5-3 except a unique orderId, fix on server later

--- public/js/deli-very.js
+++ public/js/deli-very.js
@@ -8,35 +8,17 @@ const vm = new Vue({
     data: {
         orders: {},
         info: null,
+        selectedBurgers: null,
         isShown: false,
         burgers,
     },
-    created() {
-        // socket.on('initialize', (data) => {
-        //     this.orders = data.orders;
-        // });
-
-        // socket.on('currentQueue', (data) => {
-        //     this.orders = data.orders;
-        // });
-    },
     methods: {
-        // getNext() {
-        //     const lastOrder = Object.keys(this.orders).reduce((prev, next) => Math.max(prev, next), 0);
-        //     return lastOrder + 1;
-        // },
         addOrder() {
-            const orderArray = [
-                ['Beans', 'Curry'],
-                ['Soup', 'Bread'],
-                ['Spaghetti', 'Meatballs'],
-            ];
-            const rand = Math.round(Math.random() * Math.floor(orderArray.length - 1));
-
-
             let order = this.orders[0];
-            // order.orderId = this.getNext(); // or still 'T' ????
-            order.orderItems = this.info.selectedBurgers.map(x => x.value);
+            order.orderItems = this.selectedBurgers;
+            order.info = this.info;
+            // order.orderId = this.getNextId();
+            order.orderId;
 
             socket.emit('addOrder', order);
         },
@@ -52,6 +34,7 @@ const vm = new Vue({
                     y: event.clientY - 10 - offset.y,
                 },
                 orderItems: null,
+                info: this.info,
             }]
         },
         purchase_button_pressed() {
@@ -61,8 +44,8 @@ const vm = new Vue({
                 email: document.getElementById('email').value,
                 payment_options: document.getElementById('payment_options').value,
                 gender: document.querySelector('input[name="gender"]:checked').value,
-                selectedBurgers: [...document.querySelectorAll('#burger_menu input:checked')] /* .map(x => ` ${x.value}`) */ ,
             };
+            this.selectedBurgers = [...document.querySelectorAll('#burger_menu input:checked')].map(x => x.value);
             this.addOrder();
             this.isShown = true;
         },
